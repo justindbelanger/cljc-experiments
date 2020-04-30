0cafc0aa8ac492a3d19dcde46ec1839296c1512f
Justin Belanger <justindbelanger@outlook.com>
2020-04-30 10:43:24 -0700

    Add util function for sorting translation maps

--- translations/src/translations/util.clj
+++ translations/src/translations/util.clj
@@ -9,3 +9,13 @@
     (string? locale-name)  locale-name
     :else                  (throw (ex-info "Invalid locale name"
                                            {:locale-name locale-name}))))
+
+(defn sorted-translations-map
+  "Takes an unsorted map of message IDs to translated strings,
+  provides a sorted map with the message IDs in ascending order (a -> z).
+  Makes `git diff`s of each EDN file easier."
+  [translations-map]
+  (->> translations-map
+       (mapcat identity)
+       (apply sorted-map-by (fn [k1 k2]
+                              (compare (str k1) (str k2))))))
